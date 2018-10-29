9f9a19f7592e96896be13c28bd36123041fb8d58
alx <37true@gmail.com>
2018-10-29 19:47:55 +0700

    doodle graphic editor

--- app/src/main/java/tk/alltrue/doodlz/MainActivity.java
+++ app/src/main/java/tk/alltrue/doodlz/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.doodle_fragment_menu, menu);
         return true;
     }
 
@@ -35,9 +35,9 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
         return super.onOptionsItemSelected(item);
     }
